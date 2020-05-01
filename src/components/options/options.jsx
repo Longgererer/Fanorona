41d385174ecc5149e79b31a37c5bb519f63372e7
xiaolong Hao <2072451919@qq.com>
2020-05-01 10:05:25 +0800

    Update options.jsx

--- src/components/options/options.jsx
+++ src/components/options/options.jsx
@@ -12,9 +12,9 @@ class Options extends React.Component {
       <div className={cssObj.Options}>
         {started && <Button type="danger" onClick={() => gameOver()}>Give up</Button>}
         {combo && <Button type="danger" onClick={nextTurn}>Next turn</Button>}
-        {!started && <Button type="primary" onClick={startGame} disabled={started}>Start!</Button>}
-        {started && <Button type="primary">Restart</Button>}
-        <Button type="dashed">Help</Button>
+        {!started && <Button type="primary" onClick={startGame}>Start!</Button>}
+        {started && <Button type="primary" onClick={startGame}>Restart</Button>}
+        <Button type="dashed"><a href="https://github.com/Longgererer/Fanorona#%E6%B8%B8%E6%88%8F%E8%A7%84%E5%88%99" target="black">Help</a></Button>
       </div>
     )
   }
