482102b61bd1b87e314ffaba2ecea41b6a40ccde
xiaolong Hao <2072451919@qq.com>
2021-03-08 20:41:47 +0800

    update Fanorona

---
+++ precache-manifest.c9485367ad3aef82aff0f522fff7d42b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045125951c12abca3d4a94a1936f4851",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffef2cf50a0d99b072e6",
+    "url": "./static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "a3251f9f189faa5d7e7e",
+    "url": "./static/css/main.b80f51d7.chunk.css"
+  },
+  {
+    "revision": "ffef2cf50a0d99b072e6",
+    "url": "./static/js/2.08df519d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.08df519d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3251f9f189faa5d7e7e",
+    "url": "./static/js/main.0a5b3941.chunk.js"
+  },
+  {
+    "revision": "17ef4a28dd9d9dea77b3",
+    "url": "./static/js/runtime-main.c50acc67.js"
+  }
+]);
